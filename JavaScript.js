2259407d602b738abca662b70960d727f055d377
PRosarioCordoba <130869621+PRosarioCordoba@users.noreply.github.com>
2023-05-31 19:00:07 -0300

    Estudio Contable
    Hice el menu y le cambie el color de las letras que es por defecto negro

---
+++ JavaScript.js
@@ -0,0 +1,20 @@
+let edad = prompt("ingresa tu edad");
+if (edad >= 18) {
+     alert ("Bienvenida")
+}else {
+     alert ("Bienvenida, aun no podemos asesorte contablemente")
+}
+
+function nicoleño(){
+     let nombre = prompt("ingresa tu nombre");
+     let recidente = confirm("sos de San Nicolas");
+
+     if (recidente == true) {
+          alert (nombre + " " + "tu asesoramiento e inicio de tramites seran sin costo")
+     }else {
+          alert(nombre + " " + "te esperamos en el estudio")
+     }
+}
+
+
+   
